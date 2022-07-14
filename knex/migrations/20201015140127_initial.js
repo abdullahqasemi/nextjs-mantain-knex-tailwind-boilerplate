4fa15601e38176665f125a6bd68563ec649583bf
Abdullah Qasemi <abdullahqasemi2019@gmail.com>
2022-07-14 22:24:55 +0430

    initial commit

---
+++ knex/migrations/20201015140127_initial.js
@@ -0,0 +1,17 @@
+exports.up = async function (knex) {
+	await knex.schema.createTable("todos", (table) => {
+		table.increments("id");
+		table.string("text").notNullable();
+		table.boolean("done").notNullable();
+	});
+
+	await knex("todos").insert([
+		{ text: "Buy milk", done: true },
+		{ text: "Wash car", done: false },
+		{ text: "Write Code", done: false },
+	]);
+};
+
+exports.down = async function (knex) {
+	await knex.raw("DROP TABLE todos CASCADE");
+};
